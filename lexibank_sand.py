54dfdfc6c6a33c4cec736f8596f0843c61581810
chrzyki <christoph@functor-argument.net>
2023-07-03 10:06:18 +0200

    Update code, formatting

--- lexibank_sand.py
+++ lexibank_sand.py
@@ -19,7 +19,6 @@ class CustomLanguage(pylexibank.Language):
     Sources = attr.ib(default=None)
 
 
-
 @attr.s
 class CustomLexeme(pylexibank.Lexeme):
     NumeralAnalysis = attr.ib(default=None)
@@ -34,24 +33,21 @@ class Dataset(pylexibank.Dataset):
     form_spec = pylexibank.FormSpec(replacements=[(" ", "_")])
 
     def cmd_makecldf(self, args):
-        
         args.writer.add_sources()
         concepts = {}
         for concept in self.concepts:
-            idx = concept["NUMBER"]+"_"+slug(concept["ENGLISH"])
+            idx = concept["NUMBER"] + "_" + slug(concept["ENGLISH"])
             args.writer.add_concept(
-                    ID=idx,
-                    Name=concept["ENGLISH"],
-                    Number=concept["NUMBER"],
-                    #NumberValue=concept["NUMBER_VALUE"],
-                    Concepticon_ID=concept["CONCEPTICON_ID"],
-                    Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
-                    )
+                ID=idx,
+                Name=concept["ENGLISH"],
+                Number=concept["NUMBER"],
+                Concepticon_ID=concept["CONCEPTICON_ID"],
+                Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
+            )
             concepts[concept["ENGLISH"]] = idx
         languages = args.writer.add_languages(lookup_factory="Name")
         errors = set()
-        for row in self.raw_dir.read_csv("cardinals.tsv", dicts=True,
-                delimiter="\t"):
+        for row in self.raw_dir.read_csv("cardinals.tsv", dicts=True, delimiter="\t"):
             if row["Concept"] and row["Value"]:
                 if row["Language"] not in languages:
                     errors.add(("language", row["Language"]))
@@ -59,14 +55,10 @@ class Dataset(pylexibank.Dataset):
                     errors.add(("concept", row["Concept"]))
                 else:
                     args.writer.add_forms_from_value(
-                            Language_ID=languages[row["Language"]],
-                            Parameter_ID=concepts[row["Concept"]],
-                            Value=row["Value"],
-                            Source="",
-                            #NumeralAnalysis=row["Analysis"],
-                            #Comment=row["Comment"]
-                            )
+                        Language_ID=languages[row["Language"]],
+                        Parameter_ID=concepts[row["Concept"]],
+                        Value=row["Value"],
+                        Source="",
+                    )
         for k, v in errors:
             print(k, v)
-
-
